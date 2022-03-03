9d1a6c3567f13f86b8e278ccbc5afee0f14cd5a5
MicahRCM <mcorningmyers@gmail.com>
2022-03-03 03:21:49 -0600

    Sample data ready for principal component analysis

--- analyze.R
+++ analyze.R
@@ -5,7 +5,7 @@ library(rstudioapi)
 setwd(dirname(getActiveDocumentContext()$path))
 
 # Parameters
-MIN_AUTHOR_COMMENTS <- 5
+MIN_AUTHOR_COMMENTS <- 10
 
 
 # Assign initial empty df to store author and total numbers of posts/comments
@@ -24,10 +24,11 @@ count_participation <- function(obs_csv_chunk, all_chunks) {
 
 }
 
-dir <- "./"
+dir <- "./Used Data/"
 reddit_csvs <- list.files(dir, pattern="*.csv")
 
 # Loops through all csvs in directory (reddit_csvs)
+# Pass in directory address and the data to which everything should be appended
 for (i in seq_along(reddit_csvs)) {
   all_data <- count_participation(paste0(dir, reddit_csvs[i]), all_data)
   print(i)
@@ -37,5 +38,64 @@ for (i in seq_along(reddit_csvs)) {
 valid_authors <- all_data %>%
   filter(total > MIN_AUTHOR_COMMENTS)
 
-reddit_csvs <- 
+# Input the csv location, the data everything will be appended to, and a list of valid authors
+# Outputs data with author, created, votes, subreddit, and flair.
+get_valid_author_data <- function(obs_csv_chunk, all_chunks, valid_authors) {
+  obs_csv_chunk <- read.csv(obs_csv_chunk, col.names = c("author", "created", "votes", "subreddit", "flair"))
+  valid_data <- obs_csv_chunk %>% 
+    filter(author %in% valid_authors)
+  bind_rows(valid_data, all_chunks)  
+}
 
+# Declaring typed dataframe that we will append data to 
+valid_author_comments <- data.frame(author = character(),
+                                    created = integer(),
+                                    votes = integer(),
+                                    subreddit = character(),
+                                    flair = character())
+# Test range # 
+for (i in 1:2) {
+  valid_author_comments <- get_valid_author_data(paste0(dir, reddit_csvs[i]),
+                                                  valid_author_comments,
+                                                  valid_authors$author) 
+  print(i)
+}
+
+test_valid_pcm_authors <- valid_author_comments %>% 
+  filter(subreddit == "PoliticalCompassMemes" & flair != "") %>%
+  distinct(author)
+
+test_valid_pcm_authors_all_comments <- valid_author_comments %>%
+  filter(author %in% test_valid_pcm_authors$author)
+
+test_valid_pcm_authors_all_comments$flair <- str_replace_all(test_valid_pcm_authors_all_comments$flair, "^(.*?)-( )", "")
+
+test_pcm_auth_grouped <- test_valid_pcm_authors_all_comments %>% 
+  group_by(author, subreddit, flair) %>%
+  summarise(vote_sum = sum(votes),
+         count = n())
+
+test_pcm_auth_grouped_2 <- test_pcm_auth_grouped %>% 
+  filter(subreddit == "PoliticalCompassMemes") %>%
+  distinct() %>%
+  ungroup() %>%
+  select(author, flair) %>%
+  right_join(dplyr::select((test_pcm_auth_grouped %>% 
+                              ungroup()), 
+                           -c("flair")), by = "author")
+
+test_votes_auths <- test_pcm_auth_grouped_2 %>% 
+  select(author, flair, subreddit, vote_sum) %>%
+  tidyr::pivot_wider(names_from = subreddit,
+                     values_from = vote_sum, 
+                     names_prefix = "u_",
+                     values_fn = list)
+
+test_comment_count_auths <- test_pcm_auth_grouped_2 %>%
+  select(author, flair, subreddit, count) %>%
+  pivot_wider(names_from = subreddit, 
+              values_from = count, 
+              names_prefix = "n_",
+              values_fn = list)
+
+test_final_joined_data <- left_join(test_votes_auths, select(test_comment_count_auths, -flair), by = "author")
--- compressed_stream.py
+++ compressed_stream.py
@@ -10,10 +10,11 @@ filename = "RC_2021-05"
 # Known bots and invalid authors (e.g., [deleted], AutoModerator) that won't be useful for analysis
 # bad_authors = ["[deleted]", "AutoModerator", "NFCAAOfficialRefBot", "KeepingDankMemesDank", "transcribot", "VredditDownloader", "Edgar_The_Pug_Bot", "RepostSleuthBot", "Expresstron", "transcribersofreddit", "tiny_pussy", "MTGCardFetcher", "KickItOpen_Bot", "MAGIC_EYE_BOT", "lettuce_finder", "Marketron-I", "RLCD-Bot", "nwordcountbot", "c0debrain", "SirLong3", "ChoiceEvidence", "SaveVideo", "Shakespeare-Bot"]
 
+# Reads in CSV of authors that are invalid for analysis
+bad_authors = list()
 with open ("./Params/badAuthors.csv") as file_name:
     file_read = csv.reader(file_name)
-
-bad_authors = list(file_read)
+    bad_authors = list(file_read)
 
 
 obs = []
@@ -32,10 +33,12 @@ with open(filename + ".zst", 'rb') as fh:
             for i, line in enumerate(lines[:-1]):
                 if i == 0:
                     line = previous_line + line
+                if i % 1000000 == 0:
+                    print(i)
                 object = json.loads(line)
                 # do something with the object here
-                if object['author'] not in bad_authors:   
-                    obs.append([object['author'], object['created_utc'], object['score'], object['subreddit'], object['author_flair_text']])
+                if object['author'] not in bad_authors and object['subreddit'] == 'PoliticalCompassMemes':   
+                    obs.append([object['author'], object['created_utc'], object['score'], object['subreddit'], object['author_flair_text'], object['body']])
                     if len(obs) > 5000000:
                         df = pd.DataFrame(obs)
                         df.to_csv(filename + "_a_" + str(obs_count) + ".csv", index = False)
